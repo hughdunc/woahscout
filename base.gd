51a68205177d675fd07edb725b73f8d5815270a0
Hugh Willson <willsonh29@stu.wlwv.k12.or.us>
2026-09-17 19:34:02 -0700

    FINISH BASE

--- base.gd
+++ base.gd
@@ -27,10 +27,11 @@ extends Control
 @export var red_minus: Button
 @export var red_got_first: Button
 
+@export var auto_menu: Control
 @export var menu_back: Button
-@export var auto_menu: Button
+@export var auto_switch: Button
 @export var menu_next: Button
-@export var manual_menu: Button
+@export var manual_switch: Button
 
 @export var timer: Timer
 
@@ -56,7 +57,17 @@ enum MatchPeriod {
 }
 
 
-var auto_screen_switch = true
+var short_names = {
+	MatchPeriod.AUTO: "AUTO",
+	MatchPeriod.TRANSITION: "TRANS",
+	MatchPeriod.SHIFT_1: "SHIFT 1",
+	MatchPeriod.SHIFT_2: "SHIFT 2",
+	MatchPeriod.SHIFT_3: "SHIFT 3",
+	MatchPeriod.SHIFT_4: "SHIFT 4",
+	MatchPeriod.ENDGAME: "END"
+}
+
+var auto_screen_switch = false
 var first_shift = ""
 
 var current_period: MatchPeriod
@@ -64,13 +75,15 @@ var current_menu: MatchMenu
 
 var menu_configs_red = {}
 
+var defending = false
+
 var stats = {
 	MatchMenu.AUTO: {
 		"defended": 0.0,
 		"shuttled": 0,
 		"scored": 0,
 		"no_auto": false,
-		"climb": false,
+		"climb": 0,
 	},
 	MatchMenu.TRANSITION: {
 		"defended": 0.0,
@@ -114,14 +127,14 @@ var ends = {
 }
 
 func load_menu(menu: MatchMenu):
-		var c = menu_configs_red[menu]
-		auto_menu.text = c["name"]
-		for n in c["visible"]:
-			print(n)
-			n.visible = true
-		for n in c["hidden"]:
-			n.visible = false
-		current_menu = menu
+	var c = menu_configs_red[menu]
+	auto_switch.text = c["name"]
+	for n in c["visible"]:
+		n.visible = true
+	for n in c["hidden"]:
+		n.visible = false
+	current_menu = menu
+	update_points()
 
 
 func _ready():
@@ -169,17 +182,59 @@ func _ready():
 	}
 	
 	load_menu(MatchMenu.AUTO)
-	menu_next.connect("pressed", _on_menu_next_pressed)
-	menu_back.connect("pressed", _on_menu_back_pressed)
+	menu_next.pressed.connect(_on_menu_next_pressed)
+	menu_back.pressed.connect(_on_menu_back_pressed)
 	blue_gets_first.disabled = false
 	blue_got_first.disabled = false
 	red_gets_first.disabled = false
 	red_got_first.disabled = false
-	blue_gets_first.connect("pressed", _blue_first)
-	blue_got_first.connect("pressed", _blue_first)
-	red_gets_first.connect("pressed", _red_first)
-	red_got_first.connect("pressed", _red_first)
+	blue_gets_first.pressed.connect(_blue_first)
+	blue_got_first.pressed.connect(_blue_first)
+	red_gets_first.pressed.connect(_red_first)
+	red_got_first.pressed.connect(_red_first)
 	
+	defend_button.button_down.connect(func(): defending = true)
+	defend_button.button_up.connect(func(): defending = false)
+	red_plus.pressed.connect(func(): change_points("scored", 5))
+	red_minus.pressed.connect(func(): change_points("scored", -5))
+	neutral_plus.pressed.connect(func(): change_points("shuttled", 5))
+	neutral_minus.pressed.connect(func(): change_points("shuttled", -5))
+	climb_option.item_selected.connect(func(i): change_points("climb", i))
+	no_auto.toggled.connect(func(on): change_bool("no_auto", on))
+	climb_auto.toggled.connect(func(on): change_bool("climb", 1 if on else 0))
+
+	
+	manual_switch.pressed.connect(_switch_to_manual)
+	auto_switch.pressed.connect(_switch_to_auto_switch)
+	
+	_switch_to_auto_switch()
+	
+	timer.start()
+
+func change_points(type, amt):
+	stats[current_menu][type] += amt
+	if stats[current_menu][type] < 0:
+		stats[current_menu][type] = 0
+	update_points()
+
+func change_bool(type, on):
+	stats[current_menu][type] = on
+	update_points()
+
+func update_points():
+	if red_count.is_visible_in_tree():
+		red_count.text = str(stats[current_menu]["scored"])
+	if neutral_count.is_visible_in_tree():
+		neutral_count.text = str(stats[current_menu]["shuttled"])
+	if climb_option.is_visible_in_tree():
+		climb_option.selected = stats[current_menu]["climb"]
+	if no_auto.is_visible_in_tree():
+		no_auto.button_pressed = stats[current_menu]["no_auto"]
+	if climb_auto.is_visible_in_tree():
+		climb_auto.button_pressed = stats[current_menu]["climb"] > 0
+	if defend_button.is_visible_in_tree():
+		defend_button.text = "HOLD WHILE DEFENDING\n" + format_time(ceil(stats[current_menu]["defended"]))
+
 func _on_menu_next_pressed():
 	var total_menus = MatchMenu.size()
 	var next_index = (int(current_menu) + 1) % total_menus
@@ -204,15 +259,40 @@ func _red_first():
 	red_gets_first.disabled = true
 	red_got_first.disabled = true
 
+func format_time(total_seconds: float) -> String:
+	var minutes: int = int(total_seconds) / 60
+	var seconds: int = int(total_seconds) % 60
+	
+	# Returns a padded string like "02:05" instead of "2:5"
+	return "%02d:%02d" % [minutes, seconds]
+
 func get_current_period():
-	var t = 160 - timer.time_left
+	var t = 160.0 - timer.time_left
 	for p in ends:
-		if p > t:
-			current_period = t
+		if t <= ends[p]:
+			current_period = p
+			time_until.text = format_time(ends[p] - t) + " to " + short_names[current_period + 1]
+			break
+	period.text = short_names[current_period]
+	time_left.text = format_time(timer.time_left)
+			
+func _switch_to_auto_switch():
+	if auto_screen_switch: return
+	auto_screen_switch = true
+	manual_switch.visible = true   # Show the button to switch back to manual
+	auto_menu.visible = false      # Hide the manual menu bar
+
+func _switch_to_manual():
+	if not auto_screen_switch: return
+	auto_screen_switch = false
+	manual_switch.visible = false  # Hide the manual switch button
+	auto_menu.visible = true
+
 
 func _process(delta):
+	get_current_period()
 	if auto_screen_switch and not timer.is_stopped():
-		get_current_period()
+		
 		if current_period == MatchPeriod.AUTO:
 			if current_menu != MatchMenu.AUTO:
 				load_menu(MatchMenu.AUTO)
@@ -221,32 +301,49 @@ func _process(delta):
 				load_menu(MatchMenu.TRANSITION)
 		elif current_period == MatchPeriod.SHIFT_1:
 			if first_shift == "red":
-				current_menu = MatchMenu.RED_SHIFT_1
+				if current_menu != MatchMenu.RED_SHIFT_1:
+					load_menu(MatchMenu.RED_SHIFT_1)
 			elif first_shift == "blue":
-				current_menu = MatchMenu.BLUE_SHIFT_1
+				if current_menu != MatchMenu.BLUE_SHIFT_1:
+					load_menu(MatchMenu.BLUE_SHIFT_1)
 			else:
-				current_menu = MatchMenu.WHO_GOT_FIRST
+				if current_menu != MatchMenu.WHO_GOT_FIRST:
+					load_menu(MatchMenu.WHO_GOT_FIRST)
 		elif current_period == MatchPeriod.SHIFT_2:
 			if first_shift == "red":
-				current_menu = MatchMenu.BLUE_SHIFT_1
+				if current_menu != MatchMenu.BLUE_SHIFT_1:
+					load_menu(MatchMenu.BLUE_SHIFT_1)
 			elif first_shift == "blue":
-				current_menu = MatchMenu.RED_SHIFT_1
+				if current_menu != MatchMenu.RED_SHIFT_1:
+					load_menu(MatchMenu.RED_SHIFT_1)
 			else:
-				current_menu = MatchMenu.WHO_GOT_FIRST
+				if current_menu != MatchMenu.WHO_GOT_FIRST:
+					load_menu(MatchMenu.WHO_GOT_FIRST)
 		elif current_period == MatchPeriod.SHIFT_3:
 			if first_shift == "red":
-				current_menu = MatchMenu.RED_SHIFT_2
+				if current_menu != MatchMenu.RED_SHIFT_2:
+					load_menu(MatchMenu.RED_SHIFT_2)
 			elif first_shift == "blue":
-				current_menu = MatchMenu.BLUE_SHIFT_2
+				if current_menu != MatchMenu.BLUE_SHIFT_2:
+					load_menu(MatchMenu.BLUE_SHIFT_2)
 			else:
-				current_menu = MatchMenu.WHO_GOT_FIRST
+				if current_menu != MatchMenu.WHO_GOT_FIRST:
+					load_menu(MatchMenu.WHO_GOT_FIRST)
 		elif current_period == MatchPeriod.SHIFT_4:
 			if first_shift == "red":
-				current_menu = MatchMenu.BLUE_SHIFT_2
+				if current_menu != MatchMenu.BLUE_SHIFT_2:
+					load_menu(MatchMenu.BLUE_SHIFT_2)
 			elif first_shift == "blue":
-				current_menu = MatchMenu.RED_SHIFT_2
+				if current_menu != MatchMenu.RED_SHIFT_2:
+					load_menu(MatchMenu.RED_SHIFT_2)
 			else:
-				current_menu = MatchMenu.WHO_GOT_FIRST
+				if current_menu != MatchMenu.WHO_GOT_FIRST:
+					load_menu(MatchMenu.WHO_GOT_FIRST)
 		elif current_period == MatchPeriod.ENDGAME:
 			if current_menu != MatchMenu.ENDGAME:
 				load_menu(MatchMenu.ENDGAME)
+	
+	
+	if defending:
+		stats[current_menu]["defended"] += delta
+		defend_button.text = "HOLD WHILE DEFENDING\n" + format_time(ceil(stats[current_menu]["defended"]))
--- global.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-var data = {
-	# Which alliance r u scouting?
-	"red": {
-		
-	}
-}
